2a79702fd11f9d85cd871d20c7df8cbb9c9ba2f5
Berat Guven <beratgvmo00@gmail.com>
2023-07-23 20:20:52 +0300

    Proje dosyaları eklendi

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,9 +1,5 @@
 import styles from "./styles.module.css";
 
 export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      2023 Berat Güven Tarafında tarafında yapıldı Quiz App projesi
-    </div>
-  );
+  return <div className={styles.footer}>Quiz App</div>;
 }
